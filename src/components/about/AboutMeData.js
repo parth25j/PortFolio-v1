39f9eefed21acda1517a94384edc970d3aece304
parth25j <parth2002j@gmail.com>
2025-08-02 01:21:34 +0530

    about me change

--- src/components/about/AboutMeData.js
+++ src/components/about/AboutMeData.js
@@ -3,7 +3,7 @@ const aboutMe = [
       title: "👨‍💻 About Me",
       content: `Hi, I'm Parth — a passionate full-stack developer and tech enthusiast who enjoys building dynamic and user-friendly web applications. I have a keen interest in both front-end and back-end development and love exploring how different layers of technology come together to create powerful digital experiences.
   
-  My academic journey began with Class 10 at Ryan International School and Class 12 in the Commerce stream at Sapphire International School. Driven by my growing curiosity for technology, I transitioned into computer science and completed my Bachelor of Computer Applications (BCA) in 2023, and I’m currently pursuing my Master of Computer Applications (MCA), set to graduate in 2025, both from Amity University.
+  My academic journey began with Class 10 at Ryan International School and Class 12 in the Commerce stream at Sapphire International School. Driven by my growing curiosity for technology, I transitioned into computer science and completed my Bachelor of Computer Applications (BCA) in 2023, and I have completed my Master of Computer Applications (MCA), set to graduate in 2025(been done with post-graduation), both from Amity University.
   
   Over the years, I’ve gained hands-on experience with modern web technologies and frameworks, learning how to build scalable back-end systems as well as clean, responsive user interfaces. I take pride in writing efficient code, solving problems, and continuously learning to stay updated with the latest trends in tech.
   
