9cc7247c59ee1d84c4bdd90b28edb46ba86c182d
parth25j <parth2002j@gmail.com>
2025-08-02 11:51:17 +0530

    exp sec

--- src/components/experience /ExperienceContent.js
+++ src/components/experience /ExperienceContent.js
@@ -1,17 +1,17 @@
 const content = [
     {
         id:1,
-        title:"Software Developer Frontend",
+        title:"Etelligence Technologies(Software Developer Frontend)",
         techStack: " React.js, Redux, Firebase, Tailwind CSS, JavaScript",
         discription: "Created reusable React.js components and implemented Redux for optimized state management, improvingdashboard performance by 30%,ntegrated Firebase Authentication and Firestore for secure login and real-time data, adhering to modular design,responsive UI, and WCAG accessibility standards.",
-        date: "May-2024 to June-2025"
+        date: "May-2024 to June-2024"
 ,       
 
     },
 
     {
         id:2,
-        title:"JavaScript Developer",
+        title:"Ad Link Media Pvt. Ltd.(JavaScript Developer)",
         techStack:"HTML 5, CSS 3, JavaScript, JQuery",
         discription:" Developed various slot games using JavaScript, also made various forms using JavaScript for Survey purpose alog with this contributed in making webpages for different businesses",
         date:"Jan-2023 to July-2023"
