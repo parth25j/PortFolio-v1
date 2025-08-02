0853138e47870f17bc327cd24e92a89defda8041
parth25j <parth2002j@gmail.com>
2025-08-02 14:25:10 +0530

    changed footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="w-full bg-black text-white !py-8 !px-4 ">
       <div className="max-w-6xl !mx-auto flex flex-col sm:flex-row items-center justify-between">
-        <p className="text-sm !mb-4 sm:mb-0">&copy; {new Date().getFullYear()} Parth Joshi. All rights reserved.</p>
+        <p className="text-sm !mb-4 sm:mb-0">&copy; {new Date().getFullYear()} Parth Sharma. All rights reserved.</p>
 
         <ul className="flex gap-6 text-xl">
           <li className="transition-transform duration-300 hover:scale-110 hover:text-blue-400">
